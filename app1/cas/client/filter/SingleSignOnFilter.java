8ae976ce2a6c69f57ac21534b371cca771692233
Cyrano67 <57531870+Cyrano67@users.noreply.github.com>
2020-10-26 20:46:31 +0800

    Update SingleSignOnFilter.java

--- app1/cas/client/filter/SingleSignOnFilter.java
+++ app1/cas/client/filter/SingleSignOnFilter.java
@@ -18,31 +18,17 @@ import javax.servlet.http.HttpSession;
 
 import cas.client.Constants;
 
-/**
- * Servlet Filter implementation class SingleSignOnFilter
- */
 @WebFilter(filterName="F2",urlPatterns="/*")
 public class SingleSignOnFilter implements Filter {
 	private String CAS_LOGIN_URL="http://localhost:8080/cas/login.do";
 	private String CAS_USER_URL="http://localhost:8080/cas/getUser.do";
 
-	/**
-	 * Default constructor.
-	 */
 	public SingleSignOnFilter() {
 		// TODO Auto-generated constructor stub
 	}
-
-	/**
-	 * @see Filter#destroy()
-	 */
 	public void destroy() {
 		// TODO Auto-generated method stub
 	}
-
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
@@ -50,9 +36,8 @@ public class SingleSignOnFilter implements Filter {
 		HttpSession session = httpRequest.getSession();
 		System.out.println("SingleSignOnFilter sessionID："+session.getId());
 		String CAS_ST = httpRequest.getParameter(Constants.CAS_ST);
-		if (CAS_ST != null) {
+		if (CAS_ST != null) {		//由controller产生的重定向，可直接转到资源页
 			// CAS返回ST
-			System.out.println("cas_st不为空");
 			session.setAttribute(Constants.LOCAL_ST, CAS_ST);
 			String LOCAL_SERVICE = httpRequest
 					.getParameter(Constants.LOCAL_SERVICE);
@@ -70,11 +55,11 @@ public class SingleSignOnFilter implements Filter {
 		} else {
 			String LOCAL_ST = (String) session.getAttribute(Constants.LOCAL_ST);
 			if (LOCAL_ST == null) {
-				// 跳转到CAS登录
+				// 没有在本应用或其他应用页登陆过，跳转到CAS登录
 				httpResponse.sendRedirect(CAS_LOGIN_URL + "?"
 						+ Constants.LOCAL_SERVICE + "="
 						+ httpRequest.getRequestURL());
-			} else {
+			} else {	//在其他应用登陆过
 				System.out.println("从session获取值LOCAL_USER_ID");
 				String LOCAL_USER_ID = (String) session
 						.getAttribute(Constants.LOCAL_USER_ID);
