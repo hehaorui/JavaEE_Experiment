7a1cedf6d8ad2a283856cd72f1e29229f84a2b06
Cyrano67 <57531870+Cyrano67@users.noreply.github.com>
2020-10-26 20:59:40 +0800

    Update SessionListener.java

--- app1/cas/client/SessionListener.java
+++ app1/cas/client/SessionListener.java
@@ -5,23 +5,14 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-/**
- * Application Lifecycle Listener implementation class SessionListener
- *
- */
+//监听session的创建，协助完成session的销毁
 @WebListener
 public class SessionListener implements HttpSessionListener {
 
-    /**
-     * Default constructor. 
-     */
     public SessionListener() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
-     */
     public void sessionCreated(HttpSessionEvent event) {
     	HttpSession session=event.getSession();
     	SessionMap.put(session);
@@ -29,9 +20,6 @@ public class SessionListener implements HttpSessionListener {
         System.out.printf("id为%s的会话已经创建\n", session.getId());
     }
 
-	/**
-     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
-     */
     public void sessionDestroyed(HttpSessionEvent event) {
     	 HttpSession session=event.getSession();
     	 System.out.printf("id为%s的会话已经销毁\n", session.getId());
