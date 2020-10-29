5b25d942175262e45cc6afc061b2da0db0bd9337
hxwskyi <64847779+hxwskyi@users.noreply.github.com>
2020-10-29 22:56:35 +0800

    Add files via upload

---
+++ cas/cas/server/Constrants.java
@@ -0,0 +1,7 @@
+package cas.server;
+
+public interface Constants {
+	public String CAS_TGS="CAS_TGS"; // 票据授权服务
+	public String LOCAL_SERVICE="LOCAL_SERVICE"; // 本地服务
+ 	public String CAS_ST="CAS_ST"; // 服务票据
+}
